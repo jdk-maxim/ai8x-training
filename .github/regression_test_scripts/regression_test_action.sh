e70dcc47c47a7e156c803688fa70eda2b70222cb
Jeremy Kongs <jeremy.kongs@maximintegrated.com>
2020-11-17 11:14:07 -0600

    Clean up shellcheck issues

--- .github/regression_test_scripts/regression_test_action.sh
+++ .github/regression_test_scripts/regression_test_action.sh
@@ -1,44 +1,27 @@
 #!/bin/sh -l
 
-# Reset
-Color_Off='\033[0m'       # Text Reset
-
-# Regular Colors
-Black='\033[0;30m'        # Black
-Red='\033[0;31m'          # Red
-Green='\033[0;32m'        # Green
-Yellow='\033[0;33m'       # Yellow
-Blue='\033[0;34m'         # Blue
-Purple='\033[0;35m'       # Purple
-Cyan='\033[0;36m'         # Cyan
-White='\033[0;37m'        # White
-
 # Bold fonts
-BBlack='\033[1;30m'       # Black
 BRed='\033[1;31m'         # Red
 BGreen='\033[1;32m'       # Green
-BYellow='\033[1;33m'      # Yellow
 BBlue='\033[1;34m'        # Blue
-BPurple='\033[1;35m'      # Purple
-BCyan='\033[1;36m'        # Cyan
 BWhite='\033[1;37m'       # White
 
 do_a_test_expect_success()
 {
 	echo
-	printf "\t ${BBlue}***> Running Test %s with command: %s\n${Color_Off}" "$2" "$1"
+	printf "\t %s***> Running Test %s with command: %s\n" "${BBlue}" "$2" "$1"
 	echo
 
 	# Run the command, parameter 1
 	if ! $1;
 	then
 		echo
-		printf "\t ${BRed}***> Failed to %s\n${Color_Off}" "$2"
+		printf "\t %s***> Failed to %s\n" "${BRed}" "$2"
 		echo
 		exit 1
 	else
 		echo
-		printf "\t ${BGreen}***> %s Completed, Success\n${Color_Off}" "$2"
+		printf "\t %s***> %s Completed, Success\n" "${BGreen}" "$2"
 		echo
 	fi
 }
@@ -46,7 +29,7 @@ do_a_test_expect_success()
 do_a_test_expect_failure()
 {
 	echo
-	printf "\t ${BBlue}***> Running Test %s with command: %s\n${Color_Off}" "$2" "$1"
+	printf "\t %s***> Running Test %s with command: %s\n" "${BBlue}" "$2" "$1"
 	echo
 
 	# Run the command, parameter 1
@@ -54,21 +37,21 @@ do_a_test_expect_failure()
 	if $1;
 	then
 		echo
-		printf "\t ${BRed}***> %s Completed without Error, but was expected to fail\n${Color_Off}" "$2"
+		printf "\t %s***> %s Completed without Error, but was expected to fail\n" "${BRed}" "$2"
 		echo
 		exit 1
 	else
 		echo
-		printf "\t ${BGreen}***> %s Failed as expected, Success\n${Color_Off}" "$2"
+		printf "\t %s***> %s Failed as expected, Success\n" "${BGreen}" "$2"
 		echo
 	fi
 }
 
 show_banner()
 {
-    printf "\n\n${BWhite}***********************************\n"
-    printf     " ${BWhite}$1\n"
-    printf "\n\n${BWhite}***********************************\n"
+    printf "\n\n%s***********************************\n" "${BBlue}"
+    printf     "%s%s" "${BBlue}" "$1"
+    printf "\n\n%s***********************************\n" "${BBlue}"
 }
 
 show_banner "Starting regression actions"
