3e14374d1eeb55bc22220bef4bd5b60fb22a5e85
Jeremy Kongs <jeremy.kongs@maximintegrated.com>
2020-11-19 17:06:16 -0600

    Initial commit of unicode check

---
+++ .github/linters/unicode_check.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+FILES_TO_CHECK=find . \( -iname "*.py" -o -iname "*.sh" -o -iname "*.c" -o -iname "*.h" \) -print0
+
+if xargs -0 grep -Pn '[^\0-\x7f]';
+then
+    #If we get here, that means we found at least one file with unicode in it.
+    echo
+    printf "\t \033[1;31m***> Found Unicode in files\n\033[0m"
+    echo
+    exit 1
+else
+    echo
+    printf "\t \033[1;32m***> No Unicode found in files\n\033[0m"
+    echo
+    exit 0
+fi
