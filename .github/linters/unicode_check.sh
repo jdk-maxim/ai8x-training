e4322c2490e97a4f6887b8aa2df7870797a165e8
Jeremy Kongs <jeremy.kongs@maximintegrated.com>
2020-11-19 17:22:28 -0600

    Correct lfow of unicode_check.sh

--- .github/linters/unicode_check.sh
+++ .github/linters/unicode_check.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-FILES_TO_CHECK=find . \( -iname "*.py" -o -iname "*.sh" -o -iname "*.c" -o -iname "*.h" \) -print0
+# Move to project root directory
+cd ../..
 
-if xargs -0 grep -Pn '[^\0-\x7f]';
+if find . \( -iname "*.py" -o -iname "*.sh" -o -iname "*.c" -o -iname "*.h" \) -print0 | xargs -0 grep -Pn '[^\0-\x7f]';
 then
     #If we get here, that means we found at least one file with unicode in it.
     echo
